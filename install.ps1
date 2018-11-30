4454195661409c0ccc1726e6f1178833bd53bf0b
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2018-11-30 09:20:25 +0900

    update install.ps1

--- install.ps1
+++ install.ps1
@@ -7,17 +7,6 @@ if (Get-Command scoop -eq $False) {
   Set-ExecutionPolicy RemoteSigned -s CurrentUser
   Invoke-Expression (New-Object Net.WebClient).DownloadString('https://get.scoop.sh')
   scoop install git openssl
-  scoop bucket add versions
-  scoop bucket add extras
-  scoop bucket add java
-  scoop install ag curl cacert `
-                cmake bazel `
-                oraclejdk-lts python python27 `
-                jruby ruby19 ruby nvm go `
-                vscode atom sublime-text notepadplusplus `
-                conemu msys2 flux 7zip unrar `
-                chromium firefox ccleaner winmerge `
-                gimp inkscape
 }
 
 if ((Test-Path "$HOME\.vimrc") -eq $False) {
