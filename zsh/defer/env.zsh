1c5a9f5c360924779314db530adb8bacea2fdda6
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2024-01-30 23:32:55 +0900

    update zsh settings

--- zsh/defer/env.zsh
+++ zsh/defer/env.zsh
@@ -1,9 +1,3 @@
-NODEJS_VERSION=20.11.0
-PYTHON3_VERSION=3.9.15
-PYTHON2_VERSION=2.7.18
-RUBY_VERSION=3.3.0
-GO_VERSION=1.21.6
-
 unamestr="$(uname)"
 if [[ "${unamestr}" == 'MSYS_NT-6.1' ]] ||
    [[ "${unamestr}" == 'MINGW64_NT-6.1' ]] ||
@@ -26,8 +20,4 @@ elif [[ "${unamestr}" == 'Darwin' ]]; then
 
   # flutter
   export PATH=${HOME}/development/flutter/bin:$PATH
-
-  # gcluod
-  source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
-  source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
 fi
---
+++ zsh/defer/gcloud.zsh
@@ -0,0 +1,14 @@
+unamestr="$(uname)"
+if [[ "${unamestr}" == 'MSYS_NT-6.1' ]] ||
+   [[ "${unamestr}" == 'MINGW64_NT-6.1' ]] ||
+   [[ "${unamestr}" == 'MINGW32_NT-6.1' ]] ||
+   [[ "${unamestr}" == 'MSYS_NT-10.0' ]] ||
+   [[ "${unamestr}" == 'MINGW64_NT-10.0' ]] ||
+   [[ "${unamestr}" == 'MINGW32_NT-10.0' ]]; then
+  :
+elif [[ "${unamestr}" == 'Linux' ]]; then
+  :
+elif [[ "${unamestr}" == 'Darwin' ]]; then
+  source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+  source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
+fi
