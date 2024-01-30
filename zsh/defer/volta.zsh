677436f7e9b5c49f9d43b7083232536e5b4381a4
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2024-01-30 23:28:30 +0900

    update zsh settings

--- zsh/defer/volta.zsh
+++ zsh/defer/volta.zsh
@@ -1,20 +1,5 @@
 NODEJS_VERSION=20.11.0
 
-unamestr="$(uname)"
-if [[ "${unamestr}" == 'MSYS_NT-6.1' ]] ||
-   [[ "${unamestr}" == 'MINGW64_NT-6.1' ]] ||
-   [[ "${unamestr}" == 'MINGW32_NT-6.1' ]] ||
-   [[ "${unamestr}" == 'MSYS_NT-10.0' ]] ||
-   [[ "${unamestr}" == 'MINGW64_NT-10.0' ]] ||
-   [[ "${unamestr}" == 'MINGW32_NT-10.0' ]]; then
-elif [[ "${unamestr}" == 'Linux' ]]; then
-  # volta
-  export VOLTA_HOME="$HOME/.volta"
-  export PATH="$VOLTA_HOME/bin:$PATH"
-  volta install node@v${NODEJS_VERSION}
-elif [[ "${unamestr}" == 'Darwin' ]]; then
-  # volta
-  export VOLTA_HOME="$HOME/.volta"
-  export PATH="$VOLTA_HOME/bin:$PATH"
-  volta install node@v${NODEJS_VERSION}
-fi
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+volta install node@v${NODEJS_VERSION}
