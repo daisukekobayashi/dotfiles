e8ef6a394dd300212f8ec22a8b2492031c672323
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2024-01-31 21:10:30 +0900

    update zsh settings

---
+++ zsh/defer/style.zsh
@@ -0,0 +1,8 @@
+zstyle ':chpwd:*' recent-dirs-max 500
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z-_}={A-Za-z_-}'
+zstyle ':completion:*:default' menu select=1
+zstyle ':filter-select' case-insensitive yes
+zstyle ':filter-select' extended-search yes
+zstyle ':filter-select' hist-find-no-dups yes
+zstyle ':filter-select' rotate-list yes
+
