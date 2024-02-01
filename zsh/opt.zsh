b2af62bab8bfa1d6d245ddf468e383d530e88fc3
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2024-02-01 19:15:00 +0900

    add zsh history options

--- zsh/opt.zsh
+++ zsh/opt.zsh
@@ -5,12 +5,17 @@ setopt auto_pushd
 setopt brace_ccl
 setopt correct
 #setopt extended_glob
+#
+setopt share_history
 setopt extended_history
+setopt hist_ignore_all_dups
 setopt hist_ignore_dups
 setopt hist_ignore_space
+setopt inc_append_history
+setopt hist_reduce_blanks
+
 setopt list_packed
 setopt magic_equal_subst
 setopt mark_dirs
 setopt nolistbeep
 setopt pushd_ignore_dups
-setopt share_history
