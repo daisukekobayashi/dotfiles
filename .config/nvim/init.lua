93bb9ec5bf251bd374b512eaaa1d3db6cac68b5e
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2024-04-01 19:59:33 +0900

    fix: update format function

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -508,10 +508,18 @@ local on_attach = function(_, bufnr)
   end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    -- vim.lsp.buf.format()
-    require("conform").format({ bufnr = bufnr })
-  end, { desc = 'Format current buffer with LSP' })
+  vim.api.nvim_create_user_command("Format", function(args)
+    local range = nil
+    if args.count ~= -1 then
+      local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+      range = {
+        start = { args.line1, 0 },
+        ["end"] = { args.line2, end_line:len() },
+      }
+    end
+    require("conform").format({ async = true, lsp_fallback = true, range = range })
+  end, { range = true })
+
 end
 
 -- document existing key chains
