a11029517b03d9c9de7f8da9aee24b25ab4f5754
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2024-05-13 16:14:11 +0900

    add neo-tree plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -187,6 +187,17 @@ require("lazy").setup({
     build = ":TSUpdate",
   },
 
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
