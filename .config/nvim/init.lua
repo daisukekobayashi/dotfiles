79be3fa601fe7df3577b1abde575047cf19ee2a9
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2024-08-01 15:21:19 +0900

    add markdown.nvim plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -792,6 +792,7 @@ require('lazy').setup({
         'diff',
         'html',
         'javascript',
+        'latex',
         'lua',
         'luadoc',
         'markdown',
@@ -876,6 +877,16 @@ require('lazy').setup({
       'MunifTanjim/nui.nvim',
       '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+  },
+
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    main = 'render-markdown',
+    opts = {},
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   },
 
   {
