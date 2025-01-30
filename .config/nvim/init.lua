8c7af0b67cb658707354209413d315323dec6588
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-01-30 17:37:45 +0900

    use safe_try_lint

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -909,15 +909,36 @@ require('lazy').setup({
     events = { 'BufWritePost', 'BufReadPost', 'InsertLeave' },
     config = function()
       local lint = require('lint')
+
       lint.linters_by_ft = {
         lua = { 'luacheck' },
         markdown = { 'markdownlint', 'textlint' },
         python = { 'mypy', 'flake8' },
         sh = { 'shellcheck' },
       }
+      local function safe_try_lint()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local running_linters = lint.get_running(bufnr)
+        local linters = lint.linters_by_ft[vim.bo.filetype]
+
+        if not linters then
+          return
+        end
+
+        for _, linter_name in ipairs(linters) do
+          for _, name in ipairs(running_linters) do
+            if name == linter_name then
+              return
+            end
+          end
+
+          lint.try_lint(linter_name)
+        end
+      end
+
       vim.api.nvim_create_autocmd({ 'InsertLeave', 'BufWritePost' }, {
         callback = function()
-          lint.try_lint()
+          safe_try_lint()
         end,
       })
     end,
