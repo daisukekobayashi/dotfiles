f53de474c05160a94f0bf1c59e329cb59ed55d75
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-02-04 15:30:20 +0900

    add notebook setup using quarto

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1266,6 +1266,62 @@ require('lazy').setup({
   },
 
   {
+    'quarto-dev/quarto-nvim',
+    dependencies = {
+      'jmbuhr/otter.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    ft = { 'quarto', 'markdown' },
+    opts = {
+      lspFeatures = {
+        -- NOTE: put whatever languages you want here:
+        languages = { 'r', 'python' },
+        chunks = 'all',
+        diagnostics = {
+          enabled = true,
+          triggers = { 'BufWritePost' },
+        },
+        completion = {
+          enabled = true,
+        },
+      },
+      keymap = {
+        -- NOTE: setup your own keymaps:
+        hover = 'H',
+        definition = 'gd',
+        rename = '<leader>rn',
+        references = 'gr',
+        format = '<leader>gf',
+      },
+      codeRunner = {
+        enabled = true,
+        default_method = 'molten',
+      },
+    },
+    config = function()
+      local runner = require('quarto.runner')
+      vim.keymap.set('n', '<localleader>rc', runner.run_cell, { desc = 'run cell', silent = true })
+      vim.keymap.set('n', '<localleader>ra', runner.run_above, { desc = 'run cell and above', silent = true })
+      vim.keymap.set('n', '<localleader>rA', runner.run_all, { desc = 'run all cells', silent = true })
+      vim.keymap.set('n', '<localleader>rl', runner.run_line, { desc = 'run line', silent = true })
+      vim.keymap.set('v', '<localleader>r', runner.run_range, { desc = 'run visual range', silent = true })
+      vim.keymap.set('n', '<localleader>RA', function()
+        runner.run_all(true)
+      end, { desc = 'run all cells of all languages', silent = true })
+    end,
+  },
+
+  {
+    'GCBallesteros/jupytext.nvim',
+    opts = {
+      style = 'markdown',
+      output_extension = 'md',
+      force_ft = 'markdown',
+    },
+    config = true,
+  },
+
+  {
     'benlubas/molten-nvim',
     version = '^1.0.0', -- use version <2.0.0 to avoid breaking changes
     dependencies = { '3rd/image.nvim' },
@@ -1274,6 +1330,12 @@ require('lazy').setup({
       -- these are examples, not defaults. Please see the readme
       vim.g.molten_image_provider = 'image.nvim'
       vim.g.molten_output_win_max_height = 20
+
+      vim.g.molten_auto_open_output = false
+
+      vim.g.molten_wrap_output = true
+      vim.g.molten_virt_text_output = true
+      vim.g.molten_virt_lines_off_by_1 = true
     end,
     config = function()
       vim.keymap.set('n', '<localleader>mi', ':MoltenInit<CR>', { silent = true, desc = 'Initialize the plugin' })
