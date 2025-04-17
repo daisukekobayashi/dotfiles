4290bfa3c66ff1adffc681468baad0f68654ea1c
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-04-17 15:33:25 +0900

    feat: add PowerShell support for Windows

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -74,3 +74,18 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+if vim.loop.os_uname().sysname == 'Windows_NT' then
+  local pwsh_options = {
+    shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell',
+    shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+    shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+    shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+    shellquote = '',
+    shellxquote = '',
+  }
+
+  for option, value in pairs(pwsh_options) do
+    vim.opt[option] = value
+  end
+end
