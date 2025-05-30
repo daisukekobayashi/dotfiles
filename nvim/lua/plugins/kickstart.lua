d84d8cc4f14f02f95c66dace322c22e07f277f67
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-05-30 23:30:26 +0900

    feat: add elixirls to LSP server configuration

--- nvim/lua/plugins/kickstart.lua
+++ nvim/lua/plugins/kickstart.lua
@@ -393,6 +393,7 @@ return {
         pyright = {},
         rust_analyzer = {},
         ts_ls = {},
+        elixirls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
