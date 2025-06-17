4043742864e0998ab77c8c1a9008bfc561b56acc
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-06-17 10:26:56 +0900

    feat: migrate to blink.cmp and update keymaps

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -18,3 +18,17 @@ vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
     vim.diagnostic.open_float(nil, { focus = false })
   end,
 })
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuOpen',
+  callback = function()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuClose',
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
