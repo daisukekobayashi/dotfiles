a7ae6c365ba5819a517cd7c349ea994b1339bea9
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-06-17 11:12:07 +0900

    feat: move float popup to keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,6 +7,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', 'gl', function()
+  vim.diagnostic.open_float(nil, { focus = false, border = 'rounded' })
+end, { desc = 'Show diagnostics popup' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
