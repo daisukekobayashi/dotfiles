6cb71a85c02142fb08959815d1723fc9d80d8de4
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-06-18 23:34:58 +0900

    feat: add autopairs, todo-comments, treesitter ctx

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,43 @@
+return {
+
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    lazy = true,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('nvim-treesitter.configs').setup({})
+    end,
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = { 'BufNewFile', 'BufReadPre' },
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('treesitter-context').setup({
+        enable = true,
+        multiwindow = false,
+        max_lines = 0,
+        min_window_height = 0,
+        line_numbers = true,
+        multiline_threshold = 20,
+        trim_scope = 'outer',
+        mode = 'cursor',
+        separator = nil,
+        zindex = 20,
+        on_attach = nil,
+      })
+    end,
+  },
+
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
+}
