18cca033d2c0488b77a8d7df06ad73e9d3cbed41
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-06-18 13:30:10 +0900

    feat: add noice mode to lualine_x section

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -8,6 +8,15 @@ return {
         icons_enabled = true,
         theme = 'auto',
       },
+      sections = {
+        lualine_x = {
+          {
+            require('noice').api.statusline.mode.get,
+            cond = require('noice').api.statusline.mode.has,
+            color = { fg = '#ff9e64' },
+          },
+        },
+      },
     },
   },
 
