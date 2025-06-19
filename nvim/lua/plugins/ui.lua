dddc230919fac6e3e22c44c00d49995886b6fe7b
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-06-19 12:49:47 +0900

    fix: ensure correct load order for lualine by moving noice statusline config

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -2,22 +2,27 @@ return {
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-      },
-      sections = {
-        lualine_x = {
-          {
-            require('noice').api.statusline.mode.get,
-            cond = require('noice').api.statusline.mode.has,
-            color = { fg = '#ff9e64' },
+    dependencies = {
+      'folke/noice.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('lualine').setup({
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+        },
+        sections = {
+          lualine_x = {
+            {
+              require('noice').api.statusline.mode.get,
+              cond = require('noice').api.statusline.mode.has,
+              color = { fg = '#ff9e64' },
+            },
           },
         },
-      },
-    },
+      })
+    end,
   },
 
   {
