6dae8135fd91d16cb5dba294f6554c9b7216be36
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-07-02 11:03:53 +0900

    feat: enhance toggleterm config and keymaps

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -2,7 +2,35 @@ return {
   {
     'akinsho/toggleterm.nvim',
     version = '*',
-    config = true,
+    config = function()
+      require('toggleterm').setup({
+        size = function(term)
+          if term.direction == 'horizontal' then
+            return 15
+          elseif term.direction == 'vertical' then
+            return vim.o.columns * 0.4
+          end
+        end,
+        direction = 'float',
+        float_opts = {
+          border = 'curved',
+        },
+      })
+      local Terminal = require('toggleterm.terminal').Terminal
+      local float_term = Terminal:new({ direction = 'float' })
+      local vertical_term = Terminal:new({ direction = 'vertical' })
+      local split_term = Terminal:new({ direction = 'horizontal' })
+
+      vim.keymap.set('n', '<leader>tf', function()
+        float_term:toggle()
+      end, { desc = '[T]oggle [F]loating terminal' })
+      vim.keymap.set('n', '<leader>tv', function()
+        vertical_term:toggle()
+      end, { desc = '[T]oggle [V]ertical terminal' })
+      vim.keymap.set('n', '<leader>ts', function()
+        split_term:toggle()
+      end, { desc = '[T]oggle [S]plit terminal' })
+    end,
   },
 
   {
