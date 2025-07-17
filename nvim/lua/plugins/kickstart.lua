bc56cfa59c256b29f9c3668b9ce735403a1ea842
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-07-17 13:42:36 +0900

    feat: add more language servers to kickstart config

--- nvim/lua/plugins/kickstart.lua
+++ nvim/lua/plugins/kickstart.lua
@@ -423,11 +423,21 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        -- gopls = {},
+        gopls = {},
         pyright = {},
+        ruff = {},
         rust_analyzer = {},
         ts_ls = {},
         elixirls = {},
+        bashls = {},
+        html = {},
+        cssls = {},
+        jsonls = {},
+        yamlls = {},
+        marksman = {},
+        dockerls = {},
+        emmet_language_server = {},
+
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
