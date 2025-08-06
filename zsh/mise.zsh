c35fc6d861cc9cd7ef7aea61de9cf16de04d6658
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-08-06 10:39:47 +0900

    fix: remove redundant mise shell commands

--- zsh/mise.zsh
+++ zsh/mise.zsh
@@ -13,25 +13,9 @@ if [[ "${unamestr}" == 'MSYS_NT-6.1' ]] ||
 elif [[ "${unamestr}" == 'Linux' ]]; then
   export MISE_ENV=linux
   eval "$(~/.local/bin/mise activate zsh)"
-  mise shell neovim@$(get_tool_version neovim)
-  mise shell lazygit@$(get_tool_version lazygit)
-  mise shell github-cli@$(get_tool_version github-cli)
 elif [[ "${unamestr}" == 'Darwin' ]]; then
   export MISE_ENV=macos
   eval "$(mise activate zsh)"
 fi
-
-mise shell java@$(get_tool_version java)
-mise shell clojure@$(get_tool_version clojure)
-mise shell python@$(get_tool_version python)
-mise shell node@$(get_tool_version node)
-mise shell ruby@$(get_tool_version ruby)
-mise shell go@$(get_tool_version go)
-mise shell rust@$(get_tool_version rust)
-mise shell erlang@$(get_tool_version erlang)
-mise shell elixir@$(get_tool_version elixir)
-mise shell dotnet@$(get_tool_version dotnet)
-mise shell kotolin@$(get_tool_version kotlin)
-mise shell r@$(get_tool_version r)
 
 eval "$(gh copilot alias -- zsh)"
