2c0cfdcf4b083dc62a097d2e4134cb4a1a37acd7
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-09-17 23:05:04 +0900

    feat: add git-conflict.nvim plugin

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -7,6 +7,21 @@ return {
   },
 
   {
+    'akinsho/git-conflict.nvim',
+    version = '*',
+    opts = {
+      default_mappings = true,
+      default_commands = true,
+      disable_diagnostics = false,
+      list_opener = 'copen',
+      highlights = {
+        incoming = 'DiffAdd',
+        current = 'DiffText',
+      },
+    },
+  },
+
+  {
     'kdheepak/lazygit.nvim',
     cmd = {
       'LazyGit',
