aeadb187e4b0cfdeeaaae4796c5d91800fa9ee6c
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-11-05 23:45:35 +0900

    feat: add code block textobjects and keymaps

--- nvim/lua/plugins/molten.lua
+++ nvim/lua/plugins/molten.lua
@@ -49,34 +49,42 @@ return {
       'nvim-treesitter/nvim-treesitter',
     },
     ft = { 'quarto', 'markdown' },
-    opts = {
-      lspFeatures = {
-        -- NOTE: put whatever languages you want here:
-        languages = { 'r', 'python' },
-        chunks = 'all',
-        diagnostics = {
-          enabled = true,
-          triggers = { 'BufWritePost' },
-        },
-        completion = {
-          enabled = true,
-        },
-      },
-      keymap = {
-        -- NOTE: setup your own keymaps:
-        hover = 'H',
-        definition = 'gd',
-        rename = '<leader>rn',
-        references = 'gr',
-        format = '<leader>gf',
-      },
-      codeRunner = {
-        enabled = true,
-        default_method = 'molten',
-      },
-    },
     config = function()
+      local quarto = require('quarto')
+      quarto.setup({
+
+        lspFeatures = {
+          -- NOTE: put whatever languages you want here:
+          languages = { 'r', 'python' },
+          chunks = 'all',
+          diagnostics = {
+            enabled = true,
+            triggers = { 'BufWritePost' },
+          },
+          completion = {
+            enabled = true,
+          },
+        },
+        keymap = {
+          -- NOTE: setup your own keymaps:
+          hover = 'H',
+          definition = 'gd',
+          rename = '<leader>rn',
+          references = 'gr',
+          format = '<leader>gf',
+        },
+        codeRunner = {
+          enabled = true,
+          default_method = 'molten',
+        },
+      })
       local runner = require('quarto.runner')
+      vim.keymap.set(
+        'n',
+        '<localleader>qp',
+        quarto.quartoPreview,
+        { desc = 'Preview the Quarto document', silent = true, noremap = true }
+      )
       vim.keymap.set('n', '<localleader>rc', runner.run_cell, { desc = 'run cell', silent = true })
       vim.keymap.set('n', '<localleader>ra', runner.run_above, { desc = 'run cell and above', silent = true })
       vim.keymap.set('n', '<localleader>rA', runner.run_all, { desc = 'run all cells', silent = true })
