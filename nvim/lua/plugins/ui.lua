9be8e21964df46c80602360c18021cbd98dc1b99
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-11-06 00:01:36 +0900

    feat: add live-server.nvim plugin for live reload

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -216,4 +216,11 @@ return {
       })
     end,
   },
+
+  {
+    'barrett-ruth/live-server.nvim',
+    build = 'npm install -g live-server',
+    cmd = { 'LiveServerStart', 'LiveServerStop' },
+    config = true,
+  },
 }
