4b71546079f88b8afd33ca6cbf17f09cd6d8304d
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2025-11-13 13:15:37 +0900

    feat: add tiny-inline-diagnostic and NES toggle

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -51,6 +51,7 @@ return {
     'folke/sidekick.nvim',
     opts = {
       -- add any options here
+      nes = { enabled = true },
       cli = {
         mux = {
           backend = 'tmux',
@@ -66,6 +67,26 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require('sidekick').setup(opts)
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'SidekickNesHide',
+        callback = function()
+          if disabled then
+            disabled = false
+            require('tiny-inline-diagnostic').enable()
+          end
+        end,
+      })
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'SidekickNesShow',
+        callback = function()
+          disabled = true
+          require('tiny-inline-diagnostic').disable()
+        end,
+      })
+    end,
     keys = {
       {
         '<tab>',
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -139,6 +139,16 @@ return {
   },
 
   {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy',
+    priority = 1000,
+    config = function()
+      require('tiny-inline-diagnostic').setup()
+      vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+    end,
+  },
+
+  {
     'rcarriga/nvim-notify',
     config = function()
       vim.notify = require('notify')
